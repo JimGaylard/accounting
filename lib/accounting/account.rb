90c989a2f75dac1a801c4a4b0894edb8d6c983b6
Jim Gaylard <jgaylard75@gmail.com>
2013-04-01 22:33:58 +1100

    refactored account class

--- lib/accounting/account.rb
+++ lib/accounting/account.rb
@@ -1,5 +1,9 @@
 module Accounting
   class Account
     attr_accessor :account_type
+
+    def initialize(account_type)
+      @account_type = account_type
+    end
   end
 end
--- spec/accounting/account_spec.rb
+++ spec/accounting/account_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Accounting
   describe Account do
     it "should have an Account Type" do
-      account = Account.new
+      account = Account.new(:asset)
       account.account_type = :asset
       account.account_type.should == :asset
     end
