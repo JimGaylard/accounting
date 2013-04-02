a5076655c22de85833dbbb832cdd9590a6962a0e
Jim Gaylard <jgaylard75@gmail.com>
2013-04-02 17:00:53 +1100

    working on account.rb specs

--- lib/accounting/account.rb
+++ lib/accounting/account.rb
@@ -1,9 +1,26 @@
+require 'pry'
 module Accounting
   class Account
-    attr_accessor :account_type
+    attr_accessor :account_type, :balance
 
-    def initialize(account_type)
+    def initialize(account_type, balance = 0)
       @account_type = account_type
+      @balance = balance
     end
+
+    def debit(amount)
+      binding.pry
+      case @account_type
+      when [:asset, :expense]
+        self.balance += amount
+      when [:liability, :equity, :revenue]
+        self.balance -= amount
+      end
+    end
+
+    def credit(amount)
+      self.balance -= amount
+    end
+
   end
 end
--- spec/accounting/account_spec.rb
+++ spec/accounting/account_spec.rb
@@ -2,10 +2,35 @@ require 'spec_helper'
 
 module Accounting
   describe Account do
+    let(:asset_acc) {Account.new(:asset)}
+    let(:liability_acc) {Account.new(:liability)}
+    let(:equity_acc) {Account.new(:equity)}
+    let(:revenue_acc) {Account.new(:revenue)}
+    let(:expense_acc) {Account.new(:expense)}
+
+
     it "should have an Account Type" do
-      account = Account.new(:asset)
-      account.account_type = :asset
-      account.account_type.should == :asset
+      asset_acc.account_type = :asset
+      asset_acc.account_type.should == :asset
+    end
+
+    it "should have a zero balance by default" do
+      asset_acc.balance.should == 0
+    end
+
+    it "should increase the balance  of an asset account on debit" do
+      asset_acc.debit(50)
+      asset_acc.balance.should == 50
+    end
+
+    it "should decrease the balance of an asset account on credit" do
+      asset_acc.credit(50)
+      asset_acc.balance.should == -50
+    end
+
+    it "should decrease the balance of a liability account on debit" do
+      liability_acc.debit(50)
+      liability_acc.balance.should == -50
     end
   end
 end
