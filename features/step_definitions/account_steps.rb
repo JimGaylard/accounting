0a4b69d2e52333ca9d7807d835cbb31dc2208e6e
Jim Gaylard <jgaylard75@gmail.com>
2013-04-19 16:02:23 +1000

    fixed step defs for account

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,4 +1,3 @@
-
 Given(/^a\/an (.*?) account$/) do |account_type|
   @account = Accounting::Account.new(account_type.to_sym)
   p @account
@@ -8,6 +7,6 @@ When(/^I (.*?) the account with \$(\d+)$/) do |trans_type, amount|
   puts @account.send(trans_type, amount.to_f)
 end
 
-Then(/^I should see a balance of (.*?)$/) do |new_balance|
+Then(/^I should see a balance of (\d+?)$/) do |new_balance|
   @account.balance.should == new_balance.to_f
 end
