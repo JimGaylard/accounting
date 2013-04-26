8b075fd39e1b6c27de9e83827d9677f4379a2578
Jim Gaylard <jgaylard75@gmail.com>
2013-04-26 18:10:09 +1000

    session code and tests

--- lib/accounting/session.rb
+++ lib/accounting/session.rb
@@ -7,5 +7,11 @@ module Accounting
       @output.puts "Welcome to Accounting"
       @output.puts "Enter command or type help >>"
     end
+
+    def execute(command)
+      if command == "new Account"
+        @output.puts "Please enter new Account Name"
+      end
+    end
   end
 end
